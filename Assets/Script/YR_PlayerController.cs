a63d879b34321f8b0c7c3c7cae3faa34a87286cf
je-ya <psychicartil@naver.com>
2025-03-19 21:00:12 +0900

    점프 시 느려짐, 바라보는 방향으로 캐릭터 회전 추가

--- Assets/Script/YR_PlayerController.cs
+++ Assets/Script/YR_PlayerController.cs
@@ -2,15 +2,18 @@ using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class YR_PlayerController : MonoBehaviour
 {
     Rigidbody _rb;
     [SerializeField]
     bool _isGround = true;
     Vector3 _moveInput;
-    float _speed = 10;
-    float jumpForce = 5;
-    float _rotationSpeed = 8f;
+    float _speed = 7;
+    float jumpForce = 8;
+    float _rotationSpeed;
+    float _airSpeedMultiplier = 0.5f;
+    Vector3 _initialJumpVelocity;
+
 
     void Start()
     {
@@ -19,7 +22,22 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        _rb.linearVelocity = new Vector3(_moveInput.x,0f,_moveInput.y)* _speed + Vector3.up*_rb.linearVelocity.y;
+
+        Vector3 moveVelocity = new Vector3(_moveInput.x, 0f, _moveInput.y).normalized;
+        if (_isGround)
+        {
+            _rb.linearVelocity = moveVelocity * _speed + Vector3.up * _rb.linearVelocity.y;
+
+
+        }
+        else
+        {
+            float airSpeed = _speed * _airSpeedMultiplier;
+            Vector3 targetHorizontalVelocity = _initialJumpVelocity + moveVelocity * airSpeed;
+            Vector3 currentHorizontalVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
+            Vector3 newHorizontalVelocity = Vector3.Lerp(currentHorizontalVelocity, targetHorizontalVelocity, Time.fixedDeltaTime * 5f);
+            _rb.linearVelocity = newHorizontalVelocity + Vector3.up * _rb.linearVelocity.y;
+        }
         LookForward();
     }
 
@@ -38,7 +56,7 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.phase == InputActionPhase.Performed)
+        if (context.phase == InputActionPhase.Performed && _isGround)
         {
             _isGround = false;
             _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
@@ -48,8 +66,12 @@ public class PlayerController : MonoBehaviour
 
     void LookForward()
     {
-        if (_moveInput.magnitude > 0.1f) // 작은 입력은 무시
+
+        if (_moveInput.magnitude > 0.1f)
         {
+            if (_isGround) _rotationSpeed = 8f;
+            else _rotationSpeed = 3f;
+
             Vector3 moveDirection = new Vector3(_moveInput.x, 0f, _moveInput.y).normalized;
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
             _rb.rotation = Quaternion.Slerp(_rb.rotation, targetRotation, Time.fixedDeltaTime * _rotationSpeed);
