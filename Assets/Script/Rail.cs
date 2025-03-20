68ca8fbac6d94e8d071586a872a933fc3a24c117
pdefd77 <pdefd77@gmail.com>
2025-03-20 22:46:19 +0900

    대각선 레일 기능 구현 (개선필요)
    1. Z축 속도 보정
    2. tan함수이므로 180도일 때 보정

--- Assets/Script/Rail.cs
+++ Assets/Script/Rail.cs
@@ -1,12 +1,18 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Rail : MonoBehaviour
 {
+    private Vector3 thisRotation;
+    private readonly float rad = Mathf.PI / 180f;
+
+    private void Awake()
+    {
+        thisRotation = transform.rotation.eulerAngles;
+    }
+
     private void OnTriggerEnter(Collider collider)
     {
-        collider.transform.position = new(transform.position.x, transform.position.y + 1f, collider.transform.position.z);
+        collider.transform.position = GetPosition(collider.transform.position);
         collider.transform.rotation = transform.rotation;
 
         if (collider.gameObject.CompareTag("Character"))
@@ -15,12 +21,34 @@ public class Rail : MonoBehaviour
         }
     }
 
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        collision.transform.position = new(transform.position.x, transform.position.y + 1f, collision.transform.position.z);
+        collision.transform.rotation = transform.rotation;
+
+        if (collision.gameObject.CompareTag("Character"))
+        {
+            collision.transform.GetComponent<ControlPlayer>().MoveRail_();
+        }
+    }*/
+
     private void OnCollisionStay(Collision collision)
     {
         if (collision.transform.GetComponent<ControlPlayer>().GetIsRail())
         {
-            collision.transform.position = new(transform.position.x, transform.position.y + 1f, collision.transform.position.z);
+            collision.transform.position = GetPosition(collision.transform.position);
             collision.transform.rotation = transform.rotation;
         }
+    }
+
+    private Vector3 GetPosition(Vector3 targetPosition)
+    {
+        Vector3 newPosition = transform.position;
+        float dist = targetPosition.z - transform.position.z;
+        newPosition.x += dist * Mathf.Tan(thisRotation.y * rad); // 코드 개더럽네
+        newPosition.y += -dist / Mathf.Cos(thisRotation.y * rad) * Mathf.Tan(thisRotation.x * rad) + 1; // 레일이랑 캐릭터 y스케일이 둘 다 1이니까 보정치 1
+        newPosition.z = targetPosition.z;
+
+        return newPosition;
     }
 }
