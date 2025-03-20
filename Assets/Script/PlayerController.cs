1c47a37ab88210ff77225d9d042d6c3d194168ef
pdefd77 <pdefd77@gmail.com>
2025-03-20 14:22:40 +0900

    회전 감도 수정

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -26,6 +26,13 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        MoveForward();
+        MoveSide();
+        Jump();
+    }
+
+    private void MoveForward()
+    {
         Vector3 movement = new Vector3(0f, 0f, 1 + 0.5f * input.y);
 
         movement *= movespeed;
@@ -35,39 +42,52 @@ public class PlayerController : MonoBehaviour
         //rb.position += movement/4f;
         //rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, movement.z);
         transform.Translate(movement);
+    }
 
-        rb.rotation = Quaternion.Euler(Vector3.zero);
+    private void MoveSide()
+    {
+        //rb.rotation = Quaternion.Euler(Vector3.zero);
 
         if (input.x != 0)
         {
-            rb.rotation = Quaternion.Slerp(rb.rotation, Quaternion.Euler(Vector3.up * input.x * 45f), Time.deltaTime);
+            //rb.rotation = Quaternion.Lerp(rb.rotation, Quaternion.Euler(Vector3.up * input.x * 45f), Time.deltaTime * 5f);
+
+            Vector3 rot = Quaternion.Lerp(rb.rotation, Quaternion.Euler(Vector3.up * input.x * 60f), Time.deltaTime * 5f).eulerAngles;
+
+            if (rot.y < 180f && rot.y > 45f) rot.y = 45f;
+            else if (rot.y > 180f && rot.y < 315f) rot.y = -45f;
+
+            rb.rotation = Quaternion.Euler(rot);
         }
         else
         {
-            rb.rotation = Quaternion.Slerp(rb.rotation, Quaternion.identity, Time.deltaTime * 10f);
+            rb.rotation = Quaternion.Lerp(rb.rotation, Quaternion.identity, Time.deltaTime * 10f);
         }
 
-            /*if (isGround)
-            {
-                transform.Rotate(Vector3.up * Time.deltaTime * input.x * 90f);
-            }
-            else if(input.y!=0)
-            {
-                transform.Rotate(Vector3.up * Time.deltaTime * input.x * 60f);
-            }
-            else
-            {
-                transform.Rotate(Vector3.up * Time.deltaTime * input.x * 30f);
-            }*/
+        /*if (isGround)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * input.x * 90f);
+        }
+        else if(input.y!=0)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * input.x * 60f);
+        }
+        else
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * input.x * 30f);
+        }*/
 
-            /*float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-            Vector3 movement = new Vector3(horizontal * 5f * Time.deltaTime, 0f, vertical * 5f * Time.deltaTime);
+        /*float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(horizontal * 5f * Time.deltaTime, 0f, vertical * 5f * Time.deltaTime);
 
-            //_rb.MovePosition(transform.position+movement);
+        //_rb.MovePosition(transform.position+movement);
 
-            cc.Move(movement);*/
-            Debug.Log(jump);
+        cc.Move(movement);*/
+    }
+
+    private void Jump()
+    {
         if (jump && isGround)
         {
             isGround = false;
