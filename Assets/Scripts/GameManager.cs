4df6d03afb6604409b68c01d9b2c802d660d7a88
je-ya <psychicartil@naver.com>
2025-03-22 23:31:49 +0900

    맵 추가4
    아까 저장 안해서 날아감

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -52,6 +52,7 @@ public class GameManager : MonoBehaviour
         Debug.Log("entered");
         //Rigidbody rb = player.GetComponent<Rigidbody>();
         player.transform.position = _checkPoints[_currentCheckpoint].position + Vector3.up * 3;
+
     }
 
     public void ChangeCheckpoint(int checkpointNum)
