627cf3a21a0a861e1710114246eb44d16fc4f857
pdefd77 <pdefd77@gmail.com>
2025-03-22 14:52:35 +0900

    UI 속도계 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,13 +8,38 @@ public static class Constants
 
 public class GameManager : MonoBehaviour
 {
-    // Temporary Manager (not singleton)
+    public static GameManager Instance { get; private set; } // ╫л╠шео
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown("r"))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+        if (Input.GetKeyDown(KeyCode.R)) RestartScene();
+        else if (Input.GetKeyDown(KeyCode.Escape)) QuitGame();
+    }
+
+    private void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
