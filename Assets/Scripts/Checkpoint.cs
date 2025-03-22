112b38f49a61e757f773ee87fed4cc010cf58228
YurimKim <americasso99@gmail.com>
2025-03-22 09:44:38 +0900

    fix box collider to circle, delete checkpoint collider, delete double jump

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,7 +4,7 @@ public class Checkpoint : MonoBehaviour
 {
     [SerializeField] int _checkpointNum;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.CompareTag("Player")){
             GameManagerTemp.instance.ChangeCheckpoint(_checkpointNum);
