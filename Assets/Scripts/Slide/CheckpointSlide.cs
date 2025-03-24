c59be79bd296c762810440740ad15ad4d41a15c6
pdefd77 <pdefd77@gmail.com>
2025-03-24 21:43:07 +0900

    저장 표시 기능 추가

--- Assets/Scripts/Slide/CheckpointSlide.cs
+++ Assets/Scripts/Slide/CheckpointSlide.cs
@@ -1,14 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class CheckpointSlide : MonoBehaviour
 {
     [SerializeField] int _checkpointNum;
     [SerializeField] ControlGameSlide controlGameSlide;
+    [SerializeField] RectTransform saveAlarm;
 
-    void OnTriggerEnter(Collider collision)
+    private bool isSavedFirst = false;
+
+    private void OnTriggerEnter(Collider collision)
     {
         if (!collision.gameObject.CompareTag("Player")) return;
 
         controlGameSlide.ChangeCheckpoint(_checkpointNum);
+        if(!isSavedFirst) StartCoroutine(DisplayAlarm());
+    }
+
+    private IEnumerator DisplayAlarm()
+    {
+        isSavedFirst = false;
+
+        for (int i = 0; i < 40; i++)
+        {
+            saveAlarm.position += Vector3.down * 5f;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        for (int i = 0; i < 40; i++)
+        {
+            saveAlarm.position += Vector3.up * 5f;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        yield break;
     }
 }
